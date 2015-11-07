9aa561ca1154d37a29373fd3f4289877fbc32a9a
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-11-07 19:01:17 -0430

    getting signal

--- signal_hive_reader.js
+++ signal_hive_reader.js
@@ -1,1 +1,25 @@
-alert($('table').size());
+function proccessSignal(signal){
+
+  $( signal ).each(function( index ) {
+      switch (index) {
+        case 0:
+          console.log('id', this.html);
+          break;
+        case 2:
+          console.log('updown', this.children('img').first().src);
+          break;
+        case 3:
+          console.log('expiry', this.html);
+          break;
+          case 4:
+            console.log('asset', this.html);
+            break;
+        default:
+
+      }
+  });
+}
+
+var robotSignals = $('#admin_signal_tripwire tbody tr');
+
+robotSignals.forEach(proccessSignal);
