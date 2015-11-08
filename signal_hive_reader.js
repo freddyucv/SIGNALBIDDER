2c974f11e7fddb33cca823d0a643a5bfc6a294bc
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-11-07 20:27:41 -0430

    save signal

--- signal_hive_reader.js
+++ signal_hive_reader.js
@@ -1,23 +1,43 @@
-function proccessSignal(){
+var signalsFirebase;
 
-  $( this ).each(function( index ) {
-      switch (index) {
-        case 0:
-          console.log('id', this.html);
-          break;
-        case 2:
-          console.log('updown', this.children('img').first().src);
-          break;
-        case 3:
-          console.log('expiry', this.html);
-          break;
-          case 4:
-            console.log('asset', this.html);
-            break;
-        default:
+function saveSignal(){
+    var signal = {};
 
-      }
+    $( this ).children('td').each(function( index ) {
+
+    switch (index) {
+      case 2:
+        var upSRC = "./Signal Hive Dashboard_files/call_signal.png";
+        var imgSRC = $( this ).children('img').first().attr('src');
+        signal.direction = imgSRC == upSRC ? 'up' : 'down';
+        break;
+      case 3:
+        signal.expiry = $( this ).html();
+        break;
+      case 4:
+        signal.time = $( this ).html();
+        break;
+      case 5:
+        signal.asset = $( this ).html();
+        break;
+      default:
+
+    }
   });
+
+  console.log('signal', signal);
+  signalsFirebase.push(signal);
 }
 
-$('#admin_signal_tripwire tbody tr').each(proccessSignal);
+function saveSignals(){
+  $('#admin_signal_tripwire tbody tr').each(saveSignal);
+  $('#manual_signal_tripwire tbody tr').each(saveSignal);
+}
+
+console.log('---------------------------------');
+
+$.getScript("https://cdn.firebase.com/js/client/2.3.1/firebase.js",function() {
+  console.log('##################################');
+  signalsFirebase = new Firebase('https://signalbidder.firebaseio.com/signals');
+  saveSignals();
+});
