3f361ff861382c36ae3f14ae36a69b16c1562344
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-11-07 21:14:29 -0430

    save signal only once time

--- signal_hive_reader.js
+++ signal_hive_reader.js
@@ -1,11 +1,28 @@
 var signalsFirebase;
 
+var currentSignals = [];
+var beforeSignals;
+
+function contains(signals, signal){
+  
+  for (var i = 0; i < signals.length; i++){
+    if (signals[i].signal === signal.signal && signals[i].asset === signal.asset){
+        return true;
+    }
+  }
+
+  return false;
+}
+
 function saveSignal(){
     var signal = {};
 
     $( this ).children('td').each(function( index ) {
 
     switch (index) {
+      case 1:
+        signal.signal = $( this ).find('a').html();
+        break;
       case 2:
         var upSRC = "./Signal Hive Dashboard_files/call_signal.png";
         var imgSRC = $( this ).children('img').first().attr('src');
@@ -21,17 +38,28 @@ function saveSignal(){
         signal.asset = $( this ).html();
         break;
       default:
-
     }
   });
 
-  console.log('signal', signal);
-  signalsFirebase.push(signal);
+  if (!contains(beforeSignal, signal)){
+    console.log('post signal', signal);
+    signalsFirebase.push(signal);
+  }
+
+  currentSignals.push(signal);
 }
 
 function saveSignals(){
+  console.log('111++++++++++++++++++++++++++++++++++++++++', currentSignals);
+  beforeSignal = currentSignals;
+  currentSignals = [];
+
+  console.log('2222++++++++++++++++++++++++++++++++++++++++', JSON.stringify(beforeSignal));
+
   $('#admin_signal_tripwire tbody tr').each(saveSignal);
   $('#manual_signal_tripwire tbody tr').each(saveSignal);
+
+  setTimeout(saveSignals, 1000);
 }
 
 console.log('---------------------------------');
